4247f581cce111c58a341a62144d74edd7d23cd3
tchnxcal <tchnxcal@gmail.com>
2024-12-07 20:59:43 -0500

    Book Class
    Creation of data sets + default constructor

---
+++ src/Book.java
@@ -0,0 +1,15 @@
+public class Book {
+    private String title;
+    private String author;
+    private double price;
+    private String publisher;
+    private String isbn;
+
+    public Book() {
+        this.title = "Untitled";
+        this.author = "Unknown";
+        this.price = 0.0;
+        this.publisher = "Unpublished";
+        this.isbn = "000-0-00-000000-0";
+    }
+}
